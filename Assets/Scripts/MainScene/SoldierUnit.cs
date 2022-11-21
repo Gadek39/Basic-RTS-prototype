313a092dcb894ed1c81f22305491ceee369da89f
Gadek39 <radosgost@gmail.com>
2022-11-21 10:40:14 +0100

    Soldier work update
    -Fixed Working() for a soldier
    - Still need to fine tune working and experience rates for all the units
    - Possibly need to change soldier's Working(() so safety value gets update as the work effincency increases

--- Assets/Scripts/MainScene/SoldierUnit.cs
+++ Assets/Scripts/MainScene/SoldierUnit.cs
@@ -5,27 +5,35 @@ using UnityEngine;
 public class SoldierUnit : Unit
 {
     protected float basicDefense = 5;
+    private float currentDefense;
+    private bool isGuarding;
     
     new void Update()
     {
         base.Update();
         if (!isWorking)
         {
-            startedWorking = false;
-            GameManager.instance.shield -= basicDefense+workEfficency;
+            if (isGuarding)
+            {
+                isGuarding = false;
+                GameManager.instance.shield -= currentDefense;
+            }
         }
     }
     public override void Working()
     {
         if (!startedWorking)
         {
-            StartCoroutine(OnGuard(basicDefense + workEfficency));
+            OnGuard(basicDefense + workEfficency);
         }
     }
-    IEnumerator OnGuard(float protectionRate)
+    private void OnGuard(float protectionRate)
     {
-        startedWorking = true;
-        GameManager.instance.shield += protectionRate;
-        yield return null;
+        if (!isGuarding)
+        {
+            currentDefense = protectionRate;
+            GameManager.instance.shield += currentDefense;
+            isGuarding = true;
+        }
     }
 }

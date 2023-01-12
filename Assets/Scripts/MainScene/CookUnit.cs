c7d42f8b416133c5dfb231a8f7836f4f85e4f6c0
Gadek39 <radosgost@gmail.com>
2023-01-12 17:13:06 +0100

    Saving system
    Fixed cook unit issue

--- Assets/Scripts/MainScene/CookUnit.cs
+++ Assets/Scripts/MainScene/CookUnit.cs
@@ -4,14 +4,6 @@ using UnityEngine;
 
 public class CookUnit : Unit
 {
-    
-    // Start is called before the first frame update
-    new void Start()
-    {
-
-    }
-    // Update is called once per frame
- 
     public override void Working()
     {
         if (!startedWorking)
